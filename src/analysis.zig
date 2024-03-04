97967184628f8c823b478dba6e41b7b9c072b373
nullptrdevs <16590917+nullptrdevs@users.noreply.github.com>
2024-03-04 12:36:33 -0800

    add completions for parameters of 'keyword functions'
    eg `addrspace` and `callconv`

--- src/analysis.zig
+++ src/analysis.zig
@@ -3128,6 +3128,7 @@ pub const PositionContext = union(enum) {
     var_access: offsets.Loc,
     global_error_set,
     enum_literal: offsets.Loc,
+    keyword: std.zig.Token.Tag,
     pre_label,
     label: bool,
     other,
@@ -3144,6 +3145,7 @@ pub const PositionContext = union(enum) {
             .field_access => |r| r,
             .var_access => |r| r,
             .enum_literal => |r| r,
+            .keyword => null,
             .pre_label => null,
             .label => null,
             .other => null,
@@ -3325,6 +3327,7 @@ pub fn getPositionContext(
                     .empty, .pre_label => curr_ctx.ctx = .{ .enum_literal = tok.loc },
                     .enum_literal => curr_ctx.ctx = .empty,
                     .field_access => {},
+                    .keyword => return .other, // no keyword can be `.`/`.*` accessed
                     .other => {},
                     .global_error_set => {},
                     .label => |filled| if (filled) {
@@ -3361,6 +3364,7 @@ pub fn getPositionContext(
                     }
                 },
                 .keyword_error => curr_ctx.ctx = .global_error_set,
+                .keyword_callconv, .keyword_addrspace => curr_ctx.ctx = .{ .keyword = tok.tag },
                 else => curr_ctx.ctx = .empty,
             }
 
