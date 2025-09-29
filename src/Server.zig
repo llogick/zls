962744a767e3c989f3260f2ee2ffa48cd818aa17
llogick <16590917+llogick@users.noreply.github.com>
2025-09-29 13:32:10 -0700

    TIMINGS

--- src/Server.zig
+++ src/Server.zig
@@ -1141,10 +1141,15 @@ fn changeDocumentHandler(server: *Server, _: std.mem.Allocator, notification: ty
     if (notification.contentChanges.len == 0) return;
     const handle = server.document_store.getHandle(notification.textDocument.uri) orelse return;
 
+    const ns1: i64 = @intCast(std.time.nanoTimestamp());
+
     try handle.applyContentChanges(
         notification.contentChanges,
         server.offset_encoding,
     );
+
+    const ns2: i64 = @intCast(std.time.nanoTimestamp());
+    std.log.info("D: {D}", .{ns2 - ns1});
 
     server.generateDiagnostics(handle);
 }
